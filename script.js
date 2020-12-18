bf2fabc63c86e949e5d5906eae064bde2861313a
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-17 16:34:25 -0800

    added code to my script.js. I wrote my .on(click) button function and getitem function into my script.js file.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 
-var now = (moment().format("MMMM D, YYYY"))
-$("#currentDay").text(now);
+var currentdayEl = $("#currentDay");
+currentdayEl.text(moment().format("MMMM D, YYYY"));
+var currentTime = parseInt(moment().format('k'));
+
+for (var hourCount = 9; hourCount < 18; hourCount++) {
+    var hourgrabEl = $("#hour-" + hourCount);
+    if(hourCount < currentTime) {
+        hourgrabEl.attr("class","row time-block past");
+    }
+    else if(hourCount === currentTime) {
+        hourgrabEl.attr("class","row time-block present");
+    }
+    else {
+        hourgrabEl.attr("class","row time-block future");
+    }
+}
+
+
+$(".fa-save").on("click", function(event){
+
+    var post = $("." + event.target.id).val().trim();
+    localStorage.setItem(event.target.id, post);
+
+})
+
+function getItem () {
+
+    for (var i = 1; i < 10; i ++) {
+
+        var storage = localStorage.getItem("description" + i);
+        $(".description" + i).val(storage);
+    }
+
+}
+getItem();
+
 
 
